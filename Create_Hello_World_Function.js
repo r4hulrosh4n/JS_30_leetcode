19e8d3105e0bb3d41cbf7cf9a72b04e4e1de2bf0
rahul roshan <r4hulrosh4n@gmail.com>
2024-04-08 17:24:55 +0530

    solved create Hello World Function

---
+++ Create_Hello_World_Function.js
@@ -0,0 +1,6 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World"..
+function createHelloWorld() {
+  return function() {
+    return "Hello World";
+  };
+}
